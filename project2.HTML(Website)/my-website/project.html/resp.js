fd45ad9c1ecb9549ce9ac62e2f29f33bf1db784c
Kanchisaw03 <siddheesaw@gmail.com>
2024-08-27 21:51:12 +0530

    Add files via upload

---
+++ project2.HTML(Website)/my-website/project.html/resp.js
@@ -0,0 +1,16 @@
+
+
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+rightnav = document.querySelector('.rightnav')
+navlist = document.querySelector('.nav-list')
+
+
+
+burger.addEventListener('click', ()=>{
+    rightnav.classList.toggle('v-class-responsive');
+    navlist.classList.toggle('v-class-responsive');
+    navbar.classList.toggle('h-nav-responsive');
+
+
+})
